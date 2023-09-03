2fb51af19de1d20e5a0cead03ea5b4b74ab04e53
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-09-03 09:46:31 +0300

    remove comments

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const { Command } = require("commander");
 const program = new Command();
 const contacts = require("./contacts.js");
 
-// const yargs = require("yargs");
-// const { hideBin } = require("yargs/helpers");
-
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
@@ -35,27 +32,7 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-//----------------------------
 program.parse(process.argv);
 const argv = program.opts();
-
 invokeAction(argv);
 
-//----------------------------------Ручной запуск с аргументами
-// const param = {action : 'list'}
-// const param = {action:"get", id:"rX9VXwtklXCK9nWG4xdEO"};
-// const param = { action: "add", name:"Vasya", email:"vasya@mail.com",phone: "(098)7654748"};
-// const param = { action: "remove", id: "mkEiNp7o794YCK6Pev-NJ" };
-// invokeAction(param);
-
-//----------------------------------Автоматическая обработка ком/стр
-// const actionIndex = process.argv.indexOf("--action");
-// if (actionIndex !== -1) {
-//   const action = process.argv[actionIndex + 1];
-//   invokeAction({ action });
-// }
-
-//----------------------------------Yarg для обработки ком/стр
-// const arr = hideBin(process.argv);
-// const { argv } = yargs(arr);
-// invokeAction(argv);
